f6f5be66b5c92f2074ced42350211f38d8c7e42b
Thales Oliveira S. Costa <thalesslva67@gmail.com>
2026-04-18 20:20:31 -0300

    feat(httpHeaders): create class httpHeaders for SRP(Single Responsibility Principle) for headers

---
+++ src/main/java/framework/http/response/HttpHeaders.java
@@ -0,0 +1,26 @@
+package framework.http.response;
+
+
+import java.util.Collections;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.Map;
+
+public class HttpHeaders {
+    private TreeMap<String,String> headers = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+    private static final String CONTENT_LENGTH = "Content-Length";
+    private static final Set<String> PROTECTED_HEADERS = Set.of(CONTENT_LENGTH.toLowerCase());
+
+    public Map<String, String> getHeaders() {
+        return Collections.unmodifiableMap(headers);
+    }
+
+    public void addHeader(String key, String value) {
+        if(!PROTECTED_HEADERS.contains(key.toLowerCase())) {
+            headers.put(key, value);
+        } else {
+            throw new IllegalArgumentException("Header " + key + " is protected");
+        }
+    }
+
+}
