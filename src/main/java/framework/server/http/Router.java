073156618d4f55cd19cb60cbe189d49a1e648c50
Thales Oliveira S. Costa <thalesslva67@gmail.com>
2026-04-21 15:32:48 -0300

    refactor(Router): enhance routing logic with path matching, scoring, and support for path parameters

--- src/main/java/framework/server/http/Router.java
+++ src/main/java/framework/server/http/Router.java
@@ -1,22 +1,43 @@
 package framework.server.http;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 
 public class Router {
-    private Map<String, Function<HttpRequest, HttpResponse>> routes = new ConcurrentHashMap<>();
+    private List<Route> routes = new ArrayList<>();
 
     public void addRoute(String method, String path, Function<HttpRequest, HttpResponse> handler) {
-        routes.put(method.toUpperCase() + " " + path, handler);
+        routes.add(new Route(method, path, handler));
     }
 
     public HttpResponse handle(HttpRequest request) {
-        String key = request.getMethod().toUpperCase() + " " + request.getPath();
-        Function<HttpRequest, HttpResponse> handler = routes.get(key);
-        if(handler != null) {
-            return handler.apply(request);
+        Route bestRoute = null;
+        Map<String, String> bestParams = null;
+        int bestScore = -1;
+
+        for (Route route : routes) {
+            if (!route.getMethod().equalsIgnoreCase(request.getMethod())) continue;
+
+            Map<String, String> params = route.match(request.getPath());
+
+            if (params != null) {
+                int score = route.getScore();
+                if (score > bestScore) {
+                    bestRoute = route;
+                    bestParams = params;
+                    bestScore = score;
+                }
+            }
+
         }
+
+        if (bestRoute != null) {
+            HttpRequest enriched = request.withPathParams(bestParams);
+            return bestRoute.getHandler().apply(enriched);
+        }
+
         return HttpResponse.notFound("Not Found");
     }
 }
