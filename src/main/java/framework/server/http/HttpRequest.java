7c9c79e766fe251f87be7b3834ca3cf548ca407b
Thales Oliveira S. Costa <thalesslva67@gmail.com>
2026-04-21 15:32:37 -0300

    feat(HttpRequest): add support for path parameters and utility method for injection

--- src/main/java/framework/server/http/HttpRequest.java
+++ src/main/java/framework/server/http/HttpRequest.java
@@ -9,8 +9,9 @@ public class HttpRequest {
     private final String version;
     private final HttpHeaders headers;
     private  final Map<String, String> queryParams;
+    private  final Map<String, String> pathParams;
 
-    public HttpRequest(String method, String path, String version, HttpHeaders headers, Map<String, String> queryParams) {
+    public HttpRequest(String method, String path, String version, HttpHeaders headers, Map<String, String> queryParams, Map<String, String> pathParams) {
         if(method == null) throw new IllegalArgumentException("Method cannot be null");
         if(path == null) throw new IllegalArgumentException("Path cannot be null");
         if(version == null) throw new IllegalArgumentException("Version cannot be null");
@@ -19,7 +20,12 @@ public class HttpRequest {
         this.version = version;
         this.headers = headers;
         this.queryParams = Collections.unmodifiableMap(queryParams);
+        this.pathParams = Collections.unmodifiableMap(pathParams);
 
+    }
+
+    public HttpRequest withPathParams(Map<String, String> params) {
+        return new HttpRequest(this.method, this.path, this.version, this.headers, this.queryParams, params);
     }
 
     public String getMethod() {
@@ -36,6 +42,10 @@ public class HttpRequest {
 
     public Map<String, String> getQueryParams() {
         return queryParams;
+    }
+
+    public Map<String, String> getPathParams() {
+        return pathParams;
     }
 
     public HttpHeaders getHeaders() {
