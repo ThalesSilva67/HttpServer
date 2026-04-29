5860169bbd1aaf67ad745cc04505feb809b91183
Thales Oliveira S. Costa <thalesslva67@gmail.com>
2026-04-29 16:29:01 -0300

    refactor(router): make route variables final

--- src/main/java/framework/server/http/Router.java
+++ src/main/java/framework/server/http/Router.java
@@ -6,7 +6,7 @@ import java.util.Map;
 import java.util.function.Function;
 
 public class Router {
-    private List<Route> routes = new ArrayList<>();
+    private final List<Route> routes = new ArrayList<>();
 
     public void addRoute(String method, String path, Function<HttpRequest, HttpResponse> handler) {
         routes.add(new Route(method, path, handler));
