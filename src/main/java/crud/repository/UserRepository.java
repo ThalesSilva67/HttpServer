0964953cdb220f7acd58e82db3f6a2f23a0e2d15
Thales Oliveira S. Costa <thalesslva67@gmail.com>
2026-05-01 18:42:06 -0300

    feat(userRepository): add methods save, findById, findAll and existsEmail

---
+++ src/main/java/crud/repository/UserRepository.java
@@ -0,0 +1,49 @@
+package crud.repository;
+
+import crud.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class UserRepository {
+    private static final AtomicInteger idGenerator = new AtomicInteger();
+    private static final Map<Integer, User> map = new ConcurrentHashMap<>();
+
+    public static void save(String name, String email) {
+        if(existsEmail(email)) throw new IllegalArgumentException("Email already exists");
+
+        int id = idGenerator.incrementAndGet();
+        User u = new User(String.valueOf(id), name, email);
+
+        map.put(Integer.parseInt(u.getId()), u);
+    }
+
+    public static synchronized List<String> findAll() {
+        if(map.isEmpty()) return new ArrayList<>();
+        StringBuilder sb = new StringBuilder("Users: \n");
+        for (User user : map.values()) {
+            sb.append(user).append("\n");
+        }
+
+        return List.of(sb.toString());
+    }
+
+    public static synchronized String findById(int id) {
+        if(map.isEmpty()) return null;
+
+        User user = map.get(id);
+        if(user == null) return null;
+
+        return user.toString();
+    }
+
+    private static synchronized Boolean existsEmail(String email) {
+        if(map.isEmpty()) return false;
+
+        return map.values().stream().anyMatch(u -> u.getEmail().equals(email));
+    }
+
+}
